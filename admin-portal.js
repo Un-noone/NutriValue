36a1b92f4928064606aaaf4296643c307bbc4da6
vasudhiman <vasudhiman1979@gmail.com>
2025-07-07 13:52:50 -0700

    hi

--- admin-portal.js
+++ admin-portal.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const admin = require('firebase-admin');
-const serviceAccount = require('/secrets/serviceAccountKey.json');
+const serviceAccount = require('/etc/secrets/serviceAccountKey.json');
 
 // Initialize Firebase Admin
 admin.initializeApp({
