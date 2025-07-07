a25555ee9e92b88b3df3ce8c5755ca5a62b3fa28
vasudhiman <vasudhiman1979@gmail.com>
2025-07-07 13:43:21 -0700

    Update service account key path to use environment variable for improved security

--- admin-portal.js
+++ admin-portal.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('/etc/secrets/serviceAccountKey.json');
 
 // Initialize Firebase Admin
 admin.initializeApp({
